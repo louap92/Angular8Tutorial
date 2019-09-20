19174f28ff2e559648a10f5681df32767f350c92
Pinhas <louis-alexandre.pinhas@devinci.fr>
2019-09-20 17:04:24 +0200

    sixth commit

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -20,4 +20,9 @@ export class ProductsService {
     this.http.post(`${this.uri}/add`, obj)
         .subscribe(res => console.log('Done'));
   }
+  getProducts() {
+    return this
+           .http
+           .get(`${this.uri}`);
+  }
 }
