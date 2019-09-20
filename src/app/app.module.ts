9586961774c3fba03038cd24fa1bb984fdbc62a9
Pinhas <louis-alexandre.pinhas@devinci.fr>
2019-09-20 15:17:17 +0200

    fourth commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
 
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ProductsService } from './products.service';
 
 @NgModule({
   declarations: [
@@ -21,9 +24,10 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     SlimLoadingBarModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ ProductsService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
